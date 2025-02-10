778e136ec36a01c15f5e162eecaddfa02a8e8267
Enzo Sanchez <enzojsanchez98@gmail.com>
2025-02-10 00:11:38 -0300

    add split response to assistant context

--- src/contexts/assistant.tsx
+++ src/contexts/assistant.tsx
@@ -3,11 +3,15 @@ import React, { createContext, useState } from "react";
 
 interface AssistantProps {
 	isAssistantOn: POWER_STATES
+	splitResponse: [string, string]
+	updateSplitResponse: (newResponse: [string, string]) => void
 	handleAssistant: (state: POWER_STATES) => void
 }
 
 const assistantInitialContext: AssistantProps = {
 	isAssistantOn: POWER_STATES.OFF,
+	splitResponse: ['', ''],
+	updateSplitResponse: (newResponse) => {},
 	handleAssistant: (state: POWER_STATES) => state
 }
 
@@ -15,12 +19,17 @@ export const AssistantContext = createContext(assistantInitialContext)
 
 export function AssistanteProvider ({ children }: { children: React.ReactNode }) {
 	const [isAssistantOn, setIsAssistantOn] = useState(POWER_STATES.OFF)
+	const [splitResponse, setSplitResponse] = useState<[string, string]>(['',''])
 
 	const handleAssistant = (state: POWER_STATES) => {
 		setIsAssistantOn(state)
 	}
+	
+	const updateSplitResponse = (newResponse: [string, string]) => {
+		setSplitResponse(newResponse)
+	}
 
-	return <AssistantContext.Provider value={{isAssistantOn, handleAssistant}}>
+	return <AssistantContext.Provider value={{isAssistantOn, splitResponse, updateSplitResponse, handleAssistant}}>
 		{children}
 	</AssistantContext.Provider>
 }
