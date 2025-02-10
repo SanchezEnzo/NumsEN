8a8f0c0467de7a024469c1a536d7935fa8499fa0
Enzo Sanchez <enzojsanchez98@gmail.com>
2025-02-09 22:49:32 -0300

    fix context message error

--- src/hooks/useRange.ts
+++ src/hooks/useRange.ts
@@ -1,13 +1,11 @@
-import { RangeContext } from "@/contexts/range";
-import { useContext } from "react";
+import { RangeContext } from '@/contexts/range'
+import { useContext } from 'react'
 
 export function useRange() {
 	const context = useContext(RangeContext)
 
-	if (!context) {
-		throw new Error("useRange must be used within a RangeProvider")
-	}
+	if (!context)
+		throw new Error('useRange have to be used within a RangeProvider')
 
 	return context
 }
-
