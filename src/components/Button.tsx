679cc29e01af5eb2675f38978dd73738ece58af4
Enzo Sanchez <enzojsanchez98@gmail.com>
2025-02-12 11:39:54 -0300

    add disabled prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,14 +3,16 @@ import { ReactNode } from "react"
 interface ButtonProps {
 	handleClick?: () => void
 	classButton?: string
+	disabled?: boolean
 	children: ReactNode
 }
 
-export function Button ({ handleClick, classButton, children }: ButtonProps) {
+export function Button ({ handleClick, classButton, disabled, children }: ButtonProps) {
 	return (
 		<button
 			onClick={handleClick}
 			className={`outline outline-[0.1px] px-4 py-1 rounded-lg  text-lg h-10 ${classButton}`}
+			disabled={disabled}
 		>
 			{children}
 		</button>
